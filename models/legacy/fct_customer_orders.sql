39807a32f7cd35debf06465fca442e04cbc810d6
Frédéric Darmuzey <darmuzeyfrederic@gmail.com>
2025-04-04 04:02:36 +0000

    refactor(fct_customer_orders refactor 2: centralizing logic and splitting up models)

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -1,98 +1,61 @@
 with 
 
--- Import CTEs
-
 customers as (
 
-  select * from {{ source('jaffle_shop', 'customers') }}
-
-),
-
-orders as (
-
-  select * from {{ source('jaffle_shop', 'orders') }}
-
-),
-
-payments as (
-
-  select * from {{ source('stripe', 'payment') }}
-
-),
-
--- Logical CTEs
-
-completed_payments as (
-
-    select 
-        orderid as order_id,
-        max(created) as payment_finalized_date,
-        sum(amount) / 100.0 as total_amount_paid
-    from payments
-    where status <> 'fail'
-    group by 1
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 paid_orders as (
-    select 
-        orders.id as order_id,
-        orders.user_id as customer_id,
-        orders.order_date as order_placed_at,
-        orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
-    from orders
-    left join completed_payments as p on orders.id = p.order_id
-    left join customers as c on orders.user_id = c.id ),
 
--- Final CTE
+  select * from {{ ref('int_orders') }}
+
+),
 
 final as (
-    select
-        p.order_id,
-        p.customer_id,
-        p.order_placed_at,
-        p.order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-     -- c.customer_first_name,
-     -- c.customer_last_name,
 
-        -- sales transaction sequence
-        row_number() over (order by p.order_id) as transaction_seq,
+  select
+    paid_orders.order_id,
+    paid_orders.customer_id,
+    paid_orders.order_placed_at,
+    paid_orders.order_status,
+    paid_orders.total_amount_paid,
+    paid_orders.payment_finalized_date,
+    customers.customer_first_name,
+    customers.customer_last_name,
 
-        -- customer sales sequence
-        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+    -- sales transaction sequence
+    row_number() over (order by paid_orders.order_placed_at, paid_orders.order_id) as transaction_seq,
 
-         -- new vs returning customer
-        case 
-            when (
-            rank() over (
-                partition by p.customer_id
-                order by p.order_placed_at, p.order_id
-                ) = 1
-            ) then 'new'
-        else 'return' end as nvsr,
+    -- customer sales sequence
+    row_number() over (
+        partition by paid_orders.customer_id
+        order by paid_orders.order_placed_at, paid_orders.order_id
+        ) as customer_sales_seq,
 
-        -- customer lifetime value
-        sum(p.total_amount_paid) over (
-            partition by p.customer_id
-            order by p.order_placed_at
-            ) as customer_lifetime_value,
+    -- new vs returning customer
+    case 
+      when (
+      rank() over (
+        partition by paid_orders.customer_id
+        order by paid_orders.order_placed_at, paid_orders.order_id
+        ) = 1
+      ) then 'new'
+    else 'return' end as nvsr,
 
-        -- first day of sale
-        first_value(order_placed_at) over (
-            partition by p.customer_id
-            order by p.order_placed_at
-            ) as fdos
-    from paid_orders p
- -- left join customers c on p.customer_id = c.customer_id
-    order by order_id
+    -- customer lifetime value
+    sum(paid_orders.total_amount_paid) over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(paid_orders.order_placed_at) over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) as fdos
+    from paid_orders
+    left join customers on paid_orders.customer_id = customers.customer_id
 )
-
--- Simple Select Statment
 
 select * from final
